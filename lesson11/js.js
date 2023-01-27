719f9e02d9a1a9572c07b45244f5c209afa5f849
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2023-01-27 22:51:48 +0200

    back

--- lesson11/js.js
+++ lesson11/js.js
@@ -1,12 +1,12 @@
-const btn1 = document.querySelector(".fond");
+// const btn1 = document.querySelector(".fond");
 
-const anim1 = document.querySelector(".sword1");
-const anim2 = document.querySelector(".sword2");
-const anim3 = document.querySelector(".sword3");
-const anim4 = document.querySelector(".sword4");
+// const sword1 = document.querySelector(".sword1");
+// const sword2 = document.querySelector(".sword2");
+// const sword3 = document.querySelector(".sword3");
+// const sword4 = document.querySelector(".sword4");
 
-btn1.onclick = () => {
+// btn1.onclick = () => {
    
-    anim2.style.animationPlayState = "running";
-    anim3.style.animationPlayState = "running";
-  };
+//     sword2.style.animationPlayState = "running";
+//     sword3.style.animationPlayState = "running";
+//   };
